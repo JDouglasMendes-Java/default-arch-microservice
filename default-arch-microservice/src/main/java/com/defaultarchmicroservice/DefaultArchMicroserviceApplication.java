e41b412b1c31c62f932c8160cbae88a28e940f4f
Douglas Mendes <douglasaleixomendes@gmail.com>
2022-08-29 21:35:58 -0300

    Add default project

---
+++ default-arch-microservice/src/main/java/com/defaultarchmicroservice/DefaultArchMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.defaultarchmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultArchMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultArchMicroserviceApplication.class, args);
+	}
+
+}
---
+++ default-arch-microservice/src/test/java/com/defaultarchmicroservice/DefaultArchMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defaultarchmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefaultArchMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
