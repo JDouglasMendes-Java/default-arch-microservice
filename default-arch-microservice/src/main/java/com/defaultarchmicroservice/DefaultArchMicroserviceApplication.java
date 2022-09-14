c4b559f29792abd0a40cbf0a3f7868d1ff1617bc
Douglas Mendes <douglasaleixomendes@gmail.com>
2022-09-13 21:56:17 -0300

    Fix parameter

--- default-arch-microservice/src/main/java/com/defaultarchmicroservice/DefaultArchMicroserviceApplication.java
+++ default-arch-microservice/src/main/java/com/defaultarchmicroservice/DefaultArchMicroserviceApplication.java
@@ -3,8 +3,10 @@ package com.defaultarchmicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 @EnableEurekaClient
 public class DefaultArchMicroserviceApplication {
 
---
+++ default-arch-microservice/src/main/resources/db/migration/V1__create_table_products.sql
@@ -0,0 +1,5 @@
+create table product (
+	id SERIAL PRIMARY KEY,
+  	name VARCHAR NOT NULL,
+	value NUMERIC(9,2)
+);
